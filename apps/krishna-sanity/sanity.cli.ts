636c7bf64be687f47fcb02d9066836e9c88babe6
Krishna K <krishna.kondoju007@gmail.com>
2023-02-16 22:28:12 -0500

    updated

--- apps/krishna-sanity/sanity.cli.ts
+++ apps/krishna-sanity/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'li39u3cj',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
